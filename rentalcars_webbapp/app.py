70deb448c098ee9cebd3c430d22b1d42a281cd6f
yanax <124024286+nae-x@users.noreply.github.com>
2023-07-27 14:45:20 +1200

    Update app.py

--- rentalcars_webbapp/app.py
+++ rentalcars_webbapp/app.py
@@ -3,25 +3,37 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
+from flask import session
 import re
-from datetime import datetime
 import mysql.connector
 from mysql.connector import FieldType
 import connect
 
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import bcrypt
+
+
+   
+
+
 app = Flask(__name__)
 
-dbconn = None
-connection = None
+# My secret key for extra protection)
+app.secret_key = 'ABC123'
 
-def getCursor():
-    global dbconn
-    global connection
-    connection = mysql.connector.connect(user=connect.dbuser, \
-    password=connect.dbpass, host=connect.dbhost, \
-    database=connect.dbname, autocommit=True)
-    dbconn = connection.cursor()
-    return dbconn
+# Database connection details
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '12345678'
+app.config['MYSQL_DB'] = 'rentalcars'
+app.config['MYSQL_PORT'] = 3306
+
+# Intialize MySQL
+mysql = MySQL(app)
+
+
+
 
 @app.route("/")
 def home():
@@ -31,11 +43,46 @@ def home():
 def logIn():
     return render_template("logIn.html")
 
-@app.route("/checkid", methods=["POST"])
-def checkID():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    return f"hello {username} pw {password} check ID in progress"
+@app.route("/authenticate", methods=['GET', 'POST'])
+def authenticate():
+    username = request.form['username']
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM users WHERE Username = %s', (username,))
+    account = cursor.fetchone()
+    print(account)
+    return "going through ok"
+
+    # msg = ''
+    # # Check if "username" and "password" POST requests exist (user submitted form)
+    # if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    #     # Create variables for easy access
+    #     username = request.form['username']
+    #     userPassword = request.form['password']
+    #     # Check if account exists using MySQL
+    #     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    #     cursor.execute('SELECT * FROM users WHERE Username = %s', (username,))
+    #     # Fetch one record and return result
+    #     account = cursor.fetchone()
+    #     if account is not None:
+    #         password = account['UserPassword']
+    #         if bcrypt.checkpw(userPassword.encode('utf-8'),password.encode('utf-8')):
+    #         # If account exists in accounts table in out database
+    #         # Create session data, we can access this data in other routes
+    #             session['loggedin'] = True
+    #             session['id'] = account['UserID']
+    #             session['username'] = account['Username']
+    #             # Redirect to home page
+    #             return redirect(url_for('home'))
+    #         else:
+    #             #password incorrect
+    #             msg = 'Incorrect password!'
+    #     else:
+    #         # Account doesnt exist or username incorrect
+    #         msg = 'Incorrect username'
+    # # Show the login form with message (if any)
+    # return msg
+
+
 # route and function here to deal with log in information
 # fetch username and password 
 # if usermname doesn't exist, or username and pw don't match, display msg
@@ -43,3 +90,7 @@ def checkID():
 # display links accordingly 
 
 
+
+
+# hashing password:
+# hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
