b9b9393b5821fac6c3a740eb8eb157447038bceb
yanax <124024286+nae-x@users.noreply.github.com>
2023-07-30 11:43:00 +1200

    Update app.py

--- rentalcars_webbapp/app.py
+++ rentalcars_webbapp/app.py
@@ -14,9 +14,6 @@ import MySQLdb.cursors
 import bcrypt
 
 
-   
-
-
 app = Flask(__name__)
 
 # My secret key for extra protection
@@ -35,6 +32,7 @@ mysql = MySQL(app)
 
 
 
+
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -45,11 +43,8 @@ def logIn():
 
 @app.route("/authenticate", methods=['GET', 'POST'])
 def authenticate():
-    
-
-    msg = ''
     # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    if request.method == 'POST' and request.form.get('username') and request.form.get('password'):
         # Create variables for easy access
         username = request.form['username']
         userPassword = request.form['password']
@@ -66,16 +61,98 @@ def authenticate():
                 session['loggedin'] = True
                 session['id'] = account['UserID']
                 session['username'] = account['Username']
-                # Redirect to home page
-                return redirect(url_for('home'))
+                session['userrole'] = account['UserRole']
+                # Redirect to dashboard based on user role
+                if session['userrole'] == 'admin':
+                    return redirect(url_for('adminPage'))
+                elif session['userrole'] == 'staff':
+                    return redirect(url_for('staffPage'))
+                elif session['userrole'] == 'customer':
+                    return redirect(url_for('customerPage'))
+
+
+
+
+
+
             else:
                 #password incorrect
-                msg = 'Incorrect password!'
+                msg = 'Incorrect password.'
         else:
             # Account doesnt exist or username incorrect
-            msg = 'Incorrect username'
-    # Show the login form with message (if any)
+            msg = 'Incorrect username.'
+    else:
+        msg = 'Please provide both username and password.'
+    # Show the login form with message
     return msg
+
+@app.route("/signup")
+def signUp():
+    return render_template("signUp.html")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    # Check if "username", "password" "userrole" "email" POST requests exist (user submitted form)
+    if (request.method == 'POST' 
+        and request.form.get('username') 
+        and request.form.get('password') 
+        and request.form.get('userrole') 
+        and request.form.get('email')):
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        userRole = request.form['userrole']
+        email = request.form['email']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE Username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists.'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address.'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers.'
+        elif not username or not password or not userRole or not email:
+            msg = 'Please fill out the form.'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            # print(hashed)
+            cursor.execute('INSERT INTO users (Username, UserPassword, UserRole, Email) \
+                            VALUES (%s, %s, %s, %s)', (username, hashed, userRole, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+            
+
+
+            # redirect to login page
+
+
+
+
+
+    elif request.method == 'POST':
+        # Form is empty (no POST data)
+        msg = 'Please fill out the form.'
+    # Show registration form with message (if any)
+    return msg
+
+@app.route("/adminpage")
+def adminPage():
+    return render_template("adminPage.html")
+
+@app.route("/staffpage")
+def staffPage():
+    return render_template("staffPage.html")
+
+@app.route("/customerpage")
+def customerPage():
+    return render_template("customerPage.html")
+
+
+
 
 
 
