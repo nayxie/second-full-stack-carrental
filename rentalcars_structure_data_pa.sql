af3ea8ca6b7269cc1e21f79bc2a985f6472dca8c
yanax <124024286+nae-x@users.noreply.github.com>
2023-08-09 12:29:07 +1200

    Update rentalcars_structure_data_pa.sql

--- rentalcars_structure_data_pa.sql
+++ rentalcars_structure_data_pa.sql
@@ -4,69 +4,56 @@ CREATE TABLE IF NOT EXISTS users
 (
 UserID INT auto_increment PRIMARY KEY NOT NULL,
 Username VARCHAR(30) NOT NULL,
-UserPassword VARCHAR(30) NOT NULL,
+UserPassword VARCHAR(255) NOT NULL,
 UserRole VARCHAR(30) NOT NULL,
-FirstName VARCHAR(50) NOT NULL,
-LastName VARCHAR(50) NOT NULL,
-Address VARCHAR(100) NOT NULL,
+FirstName VARCHAR(50),
+LastName VARCHAR(50),
+Address VARCHAR(100),
 Email VARCHAR(80) NOT NULL,
-Phone VARCHAR(20) NOT NULL
+Phone VARCHAR(20)
 );
 
 CREATE TABLE IF NOT EXISTS rental_cars
 (
 CarID INT auto_increment PRIMARY KEY NOT NULL,
-UserID INT NOT NULL,
 CarModel VARCHAR(50) NOT NULL,
 Registration VARCHAR(30) NOT NULL,
-ProductionYear YEAR NOT NULL,
+ProductionYear INT NOT NULL,
 Seating INT NOT NULL,
-RentalPerDay FLOAT NOT NULL,
-CarImage BLOB,
-FOREIGN KEY (UserID) REFERENCES users(UserID)
-ON UPDATE CASCADE
-ON DELETE CASCADE 
+RentalPerDay DECIMAL(10, 2) NOT NULL,
+CarImagePath VARCHAR(255)
 );
 
-INSERT INTO users VALUES 
-(1,"nayxie","123456","admin","Nanyue","Xie","12 Rosedale Road, Avondale, Auckland","nayxie@qq.com","0275642341"),
-(2,"davemoon","123456","staff","Dave","Moon","1 Bank Road, Johnsonville, Wellington","davemoon@gmail.com","02678888842"),
-(3,"jackdaniel","123456","staff","Jack","Daniel","7 Moe Avenue, Avondale, Auckland","jackdaniel@gmail.com","020042341"),
-(4,"amoregates","123456","staff","Amore","Gates","124 Moonshine Hill, Devonport, Auckland","amore12345@qq.com","0290123472"),
-(5,"adrienmckenzie","123456","customer","Adrien","McKenzie","19 Gillen Road, Green Bay, Auckland","mckenzie_adrien@qq.com","028955541"),
-(6,"cuirongli","123456","customer","Cuirong","Li","12 Dunevon Cressent, Onehunga, Auckland","jasmineli01@gmail.com","027662341"),
-(7,"lositaloane","123456","customer","Losita","Loane","123 Silverlane Avenue, Te Aro, Wellington","lositaloane@gmail.com","0215642764"),
-(8,"jakejohnson","123456","customer","Jake","Johnson","34 Daisy Street, Epsom, Auckland","jakejohnson1990@qq.com","02134530064"),
-(9,"spencerzhang","123456","customer","Spencer","Zhang","112 Watersdown Road, Mt Eden, Auckland","spencerzhang@qq.com","0251143410");
+INSERT INTO users (UserID, Username, UserPassword, UserRole, Email)
+VALUES 
+(1,"maymay","$2b$12$88pttaI2ZZY.rDdQGNdcieBhIZE8gBKN5UEK5z37kmj5ar46CB1Pe","admin","may@qq.com"),
+(2,"davemoon","$2b$12$88pttaI2ZZY.rDdQGNdcieBhIZE8gBKN5UEK5z37kmj5ar46CB1Pe","staff","davemoon@gmail.com"),
+(3,"jackdaniel","$2b$12$88pttaI2ZZY.rDdQGNdcieBhIZE8gBKN5UEK5z37kmj5ar46CB1Pe","staff","jackdaniel@gmail.com"),
+(4,"amoregates","$2b$12$88pttaI2ZZY.rDdQGNdcieBhIZE8gBKN5UEK5z37kmj5ar46CB1Pe","staff","amore12345@qq.com"),
+(5,"adrienmckenzie","$2b$12$88pttaI2ZZY.rDdQGNdcieBhIZE8gBKN5UEK5z37kmj5ar46CB1Pe","customer","mckenzie_adrien@qq.com"),
+(6,"cuirongli","$2b$12$88pttaI2ZZY.rDdQGNdcieBhIZE8gBKN5UEK5z37kmj5ar46CB1Pe","customer","jasmineli01@gmail.com"),
+(7,"lositaloane","$2b$12$88pttaI2ZZY.rDdQGNdcieBhIZE8gBKN5UEK5z37kmj5ar46CB1Pe","customer","lositaloane@gmail.com"),
+(8,"jakejohnson","$2b$12$88pttaI2ZZY.rDdQGNdcieBhIZE8gBKN5UEK5z37kmj5ar46CB1Pe","customer","jakejohnson1990@qq.com"),
+(9,"spencerzhang","$2b$12$88pttaI2ZZY.rDdQGNdcieBhIZE8gBKN5UEK5z37kmj5ar46CB1Pe","customer","spencerzhang@qq.com");
 
 INSERT INTO rental_cars VALUES 
-(1,5,"Ford","LMW374",2010,5,45,NULL),
-(2,6,"BMW","M0O123",2011,5,50,NULL),
-(3,5,"Ford","L97S10",2010,5,45,NULL),
-(4,6,"BMW","P12FG4",2007,5,47,NULL),
-(5,9,"Tesla","JH45P7",2020,5,60,NULL),
-(6,9,"Tesla","AW2379",2019,5,60,NULL),
-(7,5,"Audi","LT45P1",2008,5,43,NULL),
-(8,9,"Range Rover","LJ39T",2010,8,66,NULL),
-(9,9,"BMW","DD1374",2008,5,47,NULL),
-(10,5,"Ford","GGH394",2010,5,45,NULL),
-(11,7,"Audi","9T45RR",2008,5,43,NULL),
-(12,7,"Ford","YUU881",2010,5,45,NULL),
-(13,7,"Ford","400SAV",2011,5,45,NULL),
-(14,7,"Range Rover","TEE451",2010,8,66,NULL),
-(15,6,"BMW","QQ123A",2012,5,50,NULL),
-(16,6,"BMW","99VC6",2013,5,50,NULL),
-(17,6,"Toyota","KLJS12",2010,5,48,NULL),
-(18,8,"Toyota","JJ433",2007,5,48,NULL),
-(19,8,"Jeep","PA9997",2014,8,65,NULL),
-(20,8,"Kia","T88F66",2015,5,45,NULL);
-
-
-
-
-
-
-
-
-
-
+(1,"Ford","LMW374",2010,5,45,"img1.png"),
+(2,"BMW","M0O123",2011,5,50,"img2.png"),
+(3,"Ford","L97S10",2010,5,45,"img3.png"),
+(4,"BMW","P12FG4",2007,5,47,NULL),
+(5,"Tesla","JH45P7",2020,5,60,NULL),
+(6,"Tesla","AW2379",2019,5,60,NULL),
+(7,"Audi","LT45P1",2008,5,43,NULL),
+(8,"Range Rover","LJ39T",2010,8,66,NULL),
+(9,"BMW","DD1374",2008,5,47,NULL),
+(10,"Ford","GGH394",2010,5,45,NULL),
+(11,"Audi","9T45RR",2008,5,43,NULL),
+(12,"Ford","YUU881",2010,5,45,NULL),
+(13,"Ford","400SAV",2011,5,45,NULL),
+(14,"Range Rover","TEE451",2010,8,66,NULL),
+(15,"BMW","QQ123A",2012,5,50,NULL),
+(16,"BMW","99VC6",2013,5,50,NULL),
+(17,"Toyota","KLJS12",2010,5,48,NULL),
+(18,"Toyota","JJ433",2007,5,48,NULL),
+(19,"Jeep","PA9997",2014,8,65,NULL),
+(20,"Kia","T88F66",2015,5,45,NULL);
